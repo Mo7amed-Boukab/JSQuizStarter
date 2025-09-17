99b90f3fc7a35fcef50ba6d0b3ef7ccf983ea2b8
mohamed <mohamedboukab202@gmail.com>
2025-09-17 15:51:08 +0100

    Add timer per question, quiz time limit, and result presentation

--- index.js
+++ index.js
@@ -1,53 +1,110 @@
-
 let quizBox = document.querySelector(".quizBox");
 let options = document.querySelectorAll(".op");
-let nextBtn = document.querySelector(".quizBtn");
+let endQuiz = document.querySelector(".endQuizBoxModul");
+
+let correctResponce = document.getElementById("correctResponce");
+let incorrectResponce = document.getElementById("incorrectResponce");
+
+let scoreSpan = document.getElementById("score");
 
 let currentQuestion = 0;
-let score = 0;
+let scoreValue = 0;
+
+let correctResponceCount = 0;
+let incorrectResponceCount = 0;
+
+endQuiz.style.display = "none";
 
 function loadData() {
   const data = quizData[currentQuestion];
-  document.getElementById("quizTitle").innerText = `Question ${currentQuestion + 1} : ${data.question}`;
+  document.getElementById("quizTitle").innerText = `Question ${
+    currentQuestion + 1
+  } : ${data.question}`;
 
   options.forEach((btn, index) => {
     if (data.options[index]) {
       btn.style.display = "block";
       btn.innerText = data.options[index];
     } else {
-      btn.style.display = "none"; 
+      btn.style.display = "none";
     }
-
-    btn.style.backgroundColor = ""; 
+    btn.style.backgroundColor = "";
     btn.disabled = false;
     btn.onclick = () => checkResponce(index, btn);
   });
+
+  startTimer(5);
 }
 
 function checkResponce(selectedIndex, btn) {
-
   const correctIndex = quizData[currentQuestion].correct;
 
   if (selectedIndex === correctIndex) {
-    score += 10;
-    document.getElementById("score").innerText = score;
-    btn.style.backgroundColor = "#38b000"; 
+    scoreValue += 10;
+    correctResponceCount++;
+    scoreSpan.textContent = scoreValue;
+    btn.style.backgroundColor = "#38b000";
   } else {
+    incorrectResponceCount++;
     btn.style.backgroundColor = "red";
-    options[correctIndex].style.backgroundColor = "#38b000"; 
+    options[correctIndex].style.backgroundColor = "#38b000";
   }
 
-  options.forEach(b => (b.disabled = true));
+  options.forEach((b) => (b.disabled = true));
 }
 
-nextBtn.addEventListener("click", () => {
+function next() {
+  clearInterval(timer2Interval); 
   currentQuestion++;
   if (currentQuestion < quizData.length) {
     loadData();
   } else {
-      // modal 
+    afficherResultat();
   }
-});
+}
 
+
+function afficherResultat() {
+  quizBox.style.display = "none";
+  endQuiz.style.display = "block";
+
+  document.getElementById("resultScore").textContent = `${scoreValue} / ${
+    quizData.length * 10
+  }`;
+  correctResponce.textContent = correctResponceCount;
+  incorrectResponce.textContent = incorrectResponceCount;
+}
+
+function restartQuiz() {
+  window.location.reload();
+}
+
+let globalTimer = 30;
+let timer2Interval;
+
+let timer1 = document.getElementById("delai");
+let timer2 = document.getElementById("timer");
+
+let timer1Interval = setInterval(() => {
+  timer1.innerHTML = ` 00 : ${globalTimer}`;
+  globalTimer--;
+  if (globalTimer < 0) {
+    clearInterval(timer1Interval);
+    timer1.innerHTML = "00 : 00";
+  }
+}, 1000);
+
+function startTimer(timer) {
+
+  timer2Interval = setInterval(() => {
+    timer2.innerHTML = timer;
+    timer--;
+    if (timer < 0) {
+      clearInterval(timer2Interval);
+      timer2.innerHTML = "0";
+      next();
+    }
+  }, 1000);
+}
 
 loadData();
